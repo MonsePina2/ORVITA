ad8b1f6db351b9589d75521020d141a00d601b5b
Monserrat Piña Lira <monserratpina80@gmail.com>
2026-03-03 12:52:51 -0600

    movimiento de estrella

--- Assets/Scripts/Environment/VerticalParallax.cs
+++ Assets/Scripts/Environment/VerticalParallax.cs
@@ -14,9 +14,30 @@ namespace Environment
         }
 
         private void Update() {
-            var movement=speed*Time.deltaTime;
+            if (Mathf.Approximately(speed, 0)) return;
 
+            MoveBackground();
+
+            LoopBackgroundIfNeed();
+        }
+
+        private void MoveBackground() {
+            var movement=speed*Time.deltaTime;
             transform.Translate(movement * Vector3.up);
+        }
+
+        private void LoopBackgroundIfNeed() {
+            var offset = transform.position.y - _startPosition.y;
+
+            if (Mathf.Abs(offset)<height) return;
+
+            var direction=Mathf.Sign(offset);
+
+            var overshoot=offset-direction*height;
+
+            var overShootDir = Vector3.up * overshoot;
+
+            transform.position = _startPosition + overShootDir;
         }
     }
 }

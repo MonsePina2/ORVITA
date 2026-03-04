f8eedbfb3146b08530a47f071f058c978dbe384d
Monserrat Piña Lira <monserratpina80@gmail.com>
2026-03-04 13:06:22 -0600

    ae mueve la navesita en xy

---
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PlayerControler : MonoBehaviour
+    {
+        public void OnMove(InputAction.CallbackContext ctx) {
+            var value     = ctx.ReadValue<Vector2>();
+            var direction = new Vector3(value.x, value.y, 0);
+            transform.Translate(direction);
+        }
+    }
+}

c31e2bbc0a3b045fa3792d48840c07a3819c2f66
Monserrat Piña Lira <monserratpina80@gmail.com>
2026-03-04 13:30:24 -0600

    movimiento de la nave

--- Assets/Scripts/Player/PlayerControler.cs
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -5,10 +5,21 @@ namespace Player
 {
     public class PlayerControler : MonoBehaviour
     {
+        [SerializeField] private float speed;
+
+        [SerializeField] private Rigidbody2D rb;
+
+        private Vector3 _inputDirection;
+
+        private void FixedUpdate() {
+            var direction = _inputDirection * speed;
+            rb.AddForce(direction);
+        }
+
         public void OnMove(InputAction.CallbackContext ctx) {
             var value     = ctx.ReadValue<Vector2>();
-            var direction = new Vector3(value.x, value.y, 0);
-            transform.Translate(direction*Time.deltaTime);
+            _inputDirection = new Vector3(value.x, value.y, 0);
+
         }
     }
 }

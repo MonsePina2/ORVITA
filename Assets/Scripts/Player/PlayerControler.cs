67e571e9f2fc848cd380d01f78774f8dcf003f25
Monserrat Piña Lira <monserratpina80@gmail.com>
2026-03-04 13:11:08 -0600

    se agrega el time*deltatime para que corra igual en todas las maquinas

--- Assets/Scripts/Player/PlayerControler.cs
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -8,7 +8,7 @@ namespace Player
         public void OnMove(InputAction.CallbackContext ctx) {
             var value     = ctx.ReadValue<Vector2>();
             var direction = new Vector3(value.x, value.y, 0);
-            transform.Translate(direction);
+            transform.Translate(direction*Time.deltaTime);
         }
     }
 }

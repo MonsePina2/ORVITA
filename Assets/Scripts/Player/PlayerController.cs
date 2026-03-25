e0d67336c1c91c1a0d57d799b08d4ba68a771ccf
Monserrat Piña Lira <monserratpina80@gmail.com>
2026-03-25 12:51:05 -0600

    se escuchan los botones y el sonido de la nave

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,8 @@ namespace Player
 
         [SerializeField] private Rigidbody2D rb;
 
+        [SerializeField] private AudioSource engineAudioSourse;
+
         private Vector3 _inputDirection;
 
         private void FixedUpdate() {
@@ -19,10 +21,12 @@ namespace Player
         }
 
         public void OnMove(InputAction.CallbackContext ctx) {
-            var value     = ctx.ReadValue<Vector2>();
+            var value = ctx.ReadValue<Vector2>();
             _inputDirection = new Vector3(value.x, value.y, 0);
+            engineAudioSourse .Stop();
+            engineAudioSourse.Play();
 
-        }
+    }
 
         private void OnTriggerEnter2D(Collider2D other) {
 
